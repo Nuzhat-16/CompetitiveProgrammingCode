6e8c46c40f72547f71648a96a714cf003de94efb
Nuzhat-Nasrum <nuzhatnasrum111.gmail.com>
2024-09-07 16:46:03 +0600

    codechef practice code

---
+++ GreaterAverage.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+		int t;
+	
+	cin >> t;
+		while(t--){
+		float a,b,c;
+
+	    cin >> a>>b>>c;
+	    if(((a+b)/2)>c) {
+	        cout<<"Yes"<<endl;
+	    }
+	        else{
+	            cout<<"No"<<endl;
+	        }
+	    
+	    
+	}
+
+}
