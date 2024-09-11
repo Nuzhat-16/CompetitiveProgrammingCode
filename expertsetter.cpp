113b2a743eff62b883651ac0053eb8e0ea2361ab
Nuzhat-Nasrum <nuzhatnasrum111.gmail.com>
2024-09-11 19:11:04 +0600

    codechef problem solution

---
+++ expertsetter.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+	cin>>t;
+	while(t--){
+	    double x,y;
+	    cin>>x>>y;
+	    if((x*50)/100 <=y)
+	    cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+}
