cb260f822c555887d554789d0614137f0d4a05ed
Nuzhat-Nasrum <nuzhatnasrum111.gmail.com>
2024-09-17 18:49:08 +0600

    codechef problem solution

---
+++ chefandnextgen.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+		int t;
+	cin>>t;
+	while(t--){
+	    int a,b,x,y;
+	    cin>>a>>b>>x>>y;
+	    if(x*y>=a*b)
+	    cout<<"YES\n";
+	    else
+	    cout<<"NO\n";
+	}
+
+}
